1ab7fa42c985f9ec068fec6b08d26a1df1307fee
haoziapple <haozixiaowang@163.com>
2019-09-03 18:48:33 +0800

    添加xxl-job示例项目，执行器executor使用springboot搭建

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # xxljob-lab
 xxl-job lab project
+
+- https://gitee.com/xuxueli0323/xxl-job
+- https://github.com/xuxueli/xxl-job/
+- http://www.xuxueli.com/xxl-job
---
+++ xxl-boot/src/main/java/github/haozi/lab/xxlboot/XxlBootApplication.java
@@ -0,0 +1,13 @@
+package github.haozi.lab.xxlboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlBootApplication.class, args);
+    }
+
+}
